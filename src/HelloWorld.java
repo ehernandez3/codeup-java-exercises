b9f7aa4abbc949fed736a6ef0f69acf5a0f1e811
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-11-07 16:35:56 -0600

    added ConsoleExercises.java file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,59 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 //		System.out.println("Hello, World!");
-		System.out.print("Hello, ");
-		System.out.print("World!");
+//		System.out.print("Hello, ");
+//		System.out.print("World!");
+
+		int myFavoriteNumber = 3;
+		System.out.println(myFavoriteNumber);
+
+
+		String myString = "Namaste!";
+		System.out.println(myString);
+
+
+//		String myString = 'C'; // Gets a compiling error you cannot assign single quotes(char type) to String data type.
+//		System.out.println(myString);
+
+//		String myString = 3.14159; // Gets a compiling error you cannot assign a number to a String data type.
+//		System.out.println(myString);
+
+		float myNumber = 3.14f; //Float data type needs an 'f' at the end of the assigned value
+		System.out.println(myNumber);
+
+//		int x = 5;
+//		System.out.println(x++);
+//		System.out.println(x);
+
+//		var class;
+
+//		String theNumberThree = "three"; // Will get error due to data types
+//		Object o = theNumberThree;
+//		int three = (int) o;
+////		int three = (int) "three";
+
+//		int x = 4;
+//		x = x + 5;
+//		x += 5 // Solution
+//
+//
+//		int x = 3;
+//		int y = 4;
+//		y = y * x;
+//		y *=x ; // Solution
+//
+//
+//
+//		int x = 10;
+//		int y = 2;
+//		x = x / y;
+// 		x /= y // Solution
+//		y = y - x;
+//		y -= x;
+
+
+
+		System.out.println(Byte.MAX_VALUE);
 
 	}
 }
