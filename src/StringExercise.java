f1f29629e52b0e52d3794653b35e325d6510f97f
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-11-08 15:16:13 -0600

    completed exercise 1

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -23,8 +23,11 @@ public class StringExercise {
 //		and the amazing triple backslash \\\!
 //
 		String message = "We don't need no education\n" +
-				"We don't need no thought control\n" +
-				"Check \"this\" out!, \"s inside of \"s";
+				"We don't need no thought control\n\n" +
+				"Check \"this\" out!, \"s inside of \"s!\n\n" +
+				"In windows, the main drive is usually C:\\\n\n" +
+				"I can do backslashes \\, double backslashes \\\\,\n" +
+				"and the amazing triple backslash \\\\\\!";
 		System.out.println(message);
 //
 //
