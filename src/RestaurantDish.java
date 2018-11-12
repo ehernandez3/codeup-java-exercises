f701593e7941e7034a49be3e4ba45a92addde35e
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-11-12 17:59:48 -0600

    added and completed mini exercise files

--- src/RestaurantDish.java
+++ src/RestaurantDish.java
@@ -1,16 +1,65 @@
 public class RestaurantDish {
-    public int costInCents;
-    public String nameOfDish;
-    public boolean wouldRecommend;
-    public void eat () {
-        System.out.println("Nom nom nom!");
+    private int costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
+    private void eat () {
+    System.out.println("Nom nom nom!");
+
+    }
+    public int getCostInCents() {
+        return this.costInCents;
+        }
+    public void setCostInCents(int cents) {
+        this.costInCents = cents ;
     }
 
+    public String getNameOfDish() {
+        return this.nameOfDish;
+    }
 
+    public void setNameOfDish(String name) {
+        this.nameOfDish = name;
+    }
 
-//    public static void main (String[] arg) {
+    public boolean getWouldRecommend() {
+        return this.wouldRecommend;
+    }
+
+    public void setWouldRecommend(boolean wouldRecommend) {
+        this.wouldRecommend = wouldRecommend;
+    }
+
+    public void getEat() {
+        eat();
+    }
+    
+
+//    private String myField; //"private" means access to this is restricted
 //
+//    public String getMyField()
+//    {
+//        //include validation, logic, logging or whatever you like here
+//        return this.myField;
 //    }
+//    public void setMyField(String value)
+//    {
+//        //include more logic
+//        this.myField = value;
+//    }
+
+
+
+//-- SHORT ASSIGNMENT #2 (part 2) --
+//    Refactor your RestaurantDish and RestaurantTest classes...
+//      1) Change your public properties in your RestaurantDish class to private
+//      2) Add getters and setters for these properties (write them manually then
+//          erase them and use IntelliJ to do this quickly)
+//      3) Refactor code used in RestaurantTest to set and print these property values...
+//            ...to use your getters and setters
+//      4) Add a constructor method to RestaurantDish that will assign property...
+//            ...values based on arguments passed into the constructor
+
+
 
 
 }
--- src/RestaurantTest.java
+++ src/RestaurantTest.java
@@ -1,14 +1,21 @@
 public class RestaurantTest {
     public static void main (String[] arg) {
         RestaurantDish dish = new RestaurantDish();
-        dish.costInCents = 30;
-        dish.nameOfDish = "Pizza";
-        dish.wouldRecommend = true;
-        System.out.println(dish.costInCents);
-        System.out.println(dish.nameOfDish);
-        System.out.println(dish.wouldRecommend);
-        dish.eat();
-    }
+        dish.setCostInCents(30);
+        System.out.println(dish.getCostInCents());
+        dish.setNameOfDish("Pizza");
+        System.out.println(dish.getNameOfDish());
+        dish.setWouldRecommend(true);
+        System.out.println(dish.getWouldRecommend());
+        dish.getEat();
 
+//        dish.costInCents = 30;
+//        dish.nameOfDish = "Pizza";
+//        dish.wouldRecommend = true;
+//        System.out.println(dish.costInCents);
+//        System.out.println(dish.nameOfDish);
+//        System.out.println(dish.wouldRecommend);
+//        dish.eat();
+    }
 
 }
