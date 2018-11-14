9a211d2adbe3762594c3d0ddcbd8671618c8be57
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-11-14 10:50:40 -0600

    added User.java and UserTest.java for practicing

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,20 +1,20 @@
-package shapes;
-
-public class Circle {
-
-    private double radius;
-
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    public double getArea() {
-        return Math.PI * Math.pow(this.radius, 2);
-    }
-
-    public double getCircumference() {
-
-    }
-
-
-}
+//package shapes;
+//
+//public class Circle {
+//
+//    private double radius;
+//
+//    public Circle(double radius) {
+//        this.radius = radius;
+//    }
+//
+//    public double getArea() {
+//        return Math.PI * Math.pow(this.radius, 2);
+//    }
+//
+//    public double getCircumference() {
+//
+//    }
+//
+//
+//}
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,15 +2,15 @@ package shapes;
 import  java.util.Scanner;
 import util.Input;
 
-public class CircleApp {
-    public static void main (String[] arg) {
-        Scanner sc = new Scanner(System.in);
-        Input in = new Input(sc);
-        System.out.println("Please enter a radius length: ");
-        double radius = in.getDouble();
-
-        Circle c = new Circle(5);
-        System.out.println(c.getCircumference());
-        System.out.println(c.getArea());
-    }
-}
+//public class CircleApp {
+//    public static void main (String[] arg) {
+//        Scanner sc = new Scanner(System.in);
+////        Input in = new Input(sc);
+//        System.out.println("Please enter a radius length: ");
+//        double radius = in.getDouble();
+//
+//        Circle c = new Circle(5);
+//        System.out.println(c.getCircumference());
+//        System.out.println(c.getArea());
+//    }
+//}
